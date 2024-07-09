346c4ff3f1fc02e76314640811562fa9256b2cfb
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-07-09 17:31:30 +0200

    courses pages

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { getFeedback, getIndexHtml, postFeedback } from "./controllers.js";
+import {
+  getFeedback,
+  getIndexHtml,
+  postFeedback,
+  createCourse,
+  deleteCourse,
+  showCourse,
+  showForm,
+  updateCourse,
+} from "./controllers.js";
 
 const app = new Hono();
 
 app.get("/", getIndexHtml);
 app.get("/feedbacks/:key", getFeedback);
 app.post("/feedbacks/:key", postFeedback);
+
+app.get("/courses", showForm);
+app.get("/courses/:id", showCourse);
+app.post("/courses", createCourse);
+app.post("/courses/:id", updateCourse);
+app.post("/courses/:id/delete", deleteCourse);
 
 export default app;
--- controllers.js
+++ controllers.js
@@ -1,5 +1,6 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { getCount, incrementCount } from "./feedbacks.js";
+import * as courseService from "./courses.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
@@ -21,4 +22,38 @@ export const postFeedback = async (c) => {
   const key = c.req.param("key");
   await incrementCount(key);
   return c.redirect("/");
+};
+
+// * ----------------------------------------------------------
+
+export const showForm = async (c) => {
+  return c.html(
+    eta.render("courses.eta", { courses: await courseService.listCourses() })
+  );
+};
+
+export const createCourse = async (c) => {
+  const body = await c.req.parseBody();
+  await courseService.createCourse(body);
+  return c.redirect("/courses");
+};
+
+export const showCourse = async (c) => {
+  const id = c.req.param("id");
+  return c.html(
+    eta.render("course.eta", { course: await courseService.getCourse(id) })
+  );
+};
+
+export const updateCourse = async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.parseBody();
+  await courseService.updateCourse(id, body);
+  return c.redirect(`/courses/${id}`);
+};
+
+export const deleteCourse = async (c) => {
+  const id = c.req.param("id");
+  await courseService.deleteCourse(id);
+  return c.redirect("/courses");
 };

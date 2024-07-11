2395bb6b95860261d46c2644d559d1914d7892b1
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-07-11 13:34:52 +0200

    added form validator

--- controllers.js
+++ controllers.js
@@ -1,10 +1,21 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 import { getCount, incrementCount } from "./feedbacks.js";
 import * as courseService from "./courses.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const options = ["Poor", "Fair", "Good", "Very good", "Excellent"];
+
+const validator = z.object({
+  name: z
+    .string({
+      message: "The course name should be a string of at least 4 characters.",
+    })
+    .min(4, {
+      message: "The course name should be a string of at least 4 characters.",
+    }),
+});
 
 export const getFeedback = async (c) => {
   const id = c.req.param("id");
@@ -22,14 +33,23 @@ export const postFeedback = async (c) => {
 
 // * ----------------------------------------------------------
 
-export const showForm = async (c) => {
+export const showForm = async (c, extra_args = {}) => {
   return c.html(
-    eta.render("courses.eta", { courses: await courseService.listCourses() })
+    eta.render("courses.eta", {
+      courses: await courseService.listCourses(),
+      ...extra_args,
+    })
   );
 };
 
 export const createCourse = async (c) => {
   const body = await c.req.parseBody();
+  const validationResult = validator.safeParse(body);
+  if (!validationResult.success) {
+    const errors = validationResult.error.format();
+    console.log(errors);
+    return showForm(c, { ...body, errors });
+  }
   await courseService.createCourse(body);
   return c.redirect("/courses");
 };
